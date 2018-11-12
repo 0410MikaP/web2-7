f4cddb02ec928770617fe8414b43ccc0ea077cdc
0410MikaP <38421123+0410MikaP@users.noreply.github.com>
2018-11-12 17:58:35 +0900

    Create default.js

---
+++ js/default.js
@@ -0,0 +1,9 @@
+//確認ダイアログを表示
+var result=window.confirm('準備OK?');
+//結果を表示
+if(result){
+document.getElementById('choice').textContent='OKが押されました';
+}eles{
+document.getElementById('choice').textContent='キャンセルが押されました';
+}
+//答えを用意
