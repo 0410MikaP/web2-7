716df3cecfde5c609cd2fa2bff518a58cb35982d
0410MikaP <38421123+0410MikaP@users.noreply.github.com>
2018-11-12 18:09:34 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,9 +1,25 @@
 //確認ダイアログを表示
-var result=window.confirm('準備OK?');
+var result = window.confirm('準備OK?');
 //結果を表示
 if(result){
-document.getElementById('choice').textContent='OKが押されました';
+  document.getElementById('choice').textContent='OKが押されました';
 }eles{
-document.getElementById('choice').textContent='キャンセルが押されました';
+  document.getElementById('choice').textContent='キャンセルが押されました';
 }
 //答えを用意
+var number = Math.floor(Math.random()*3);
+var answer = parseInt(window.prompt('数あてゲーム。0～2の数字を入力してください！'));
+
+var mesage;
+if(answer === number){
+  message = 'あたり！';
+}else if(answer < number){
+  message = '残念！もっと大きいです';
+}eles if(number < answer){
+  message = '残念！もっと小さいです';
+}eles{
+  message = '0～3の数字を入力してください';
+}
+
+//結果を表示
+document.getElementById('choice').textContent = message;
